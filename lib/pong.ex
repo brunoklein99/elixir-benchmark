e50b059a9d4bd43e84e7d2f73731a52460953028
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-04-08 15:47:18 -0300

    logs

--- lib/pong.ex
+++ lib/pong.ex
@@ -6,7 +6,9 @@ defmodule Pong do
   defp start_internal do
     receive do
       {pid, msg_id, _msg} ->
+        IO.puts "received #{msg_id}"
         send pid, {:ok, msg_id}
+        IO.puts "sent #{msg_id}"
         start_internal()
     end
   end
