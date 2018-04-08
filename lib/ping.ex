b2b30139e6e023269005cb8285187368857d18d8
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-04-08 15:34:43 -0300

    changed message count

--- lib/ping.ex
+++ lib/ping.ex
@@ -4,14 +4,14 @@ defmodule Ping do
     start_internal(pid, 0, msg)
   end
 
-  defp start_internal(_pid, 10_000_000, _msg) do
+  defp start_internal(_pid, 1000, _msg) do
     IO.puts "finished"
   end
 
   defp start_internal(pid, msg_id, msg) do
     send pid, {self(), msg_id, msg}
     receive do
-      {:ok, ^msg_id} -> nil
+      {:ok, ^msg_id} -> IO.puts "received #{msg_id}"
     end
     start_internal(pid, msg_id + 1, msg)
   end
